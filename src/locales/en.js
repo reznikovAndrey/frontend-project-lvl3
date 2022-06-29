096a08b2eb3df042d5fdee3b630bc8b90f6d6a3a
Andrey Reznikov <reznikovandrusha@gmail.com>
2022-06-30 02:48:26 +0500

    install `i18next`, add texts for form errors

---
+++ src/locales/en.js
@@ -0,0 +1,12 @@
+export default {
+  translation: {
+    form: {
+      successMessage: 'Rss was successfully loaded!',
+      errorMessages: {
+        emptyInput: 'Please, provide RSS link.',
+        invalidUrl: 'RSS link must be a valid URL.',
+        linkAlreadyWasLoaded: 'This RSS link was already loaded.',
+      },
+    },
+  },
+};
